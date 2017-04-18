97730c84e9ee2ca11920b3476dce6533dbbabcb8
ArtOfCode <owen.jenkins2903@gmail.com>
2017-04-18 12:11:55 +0100

    Update gem for current Ruby

--- aescrypt.gemspec
+++ aescrypt.gemspec
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 Gem::Specification.new do |gem|
-  gem.authors       = ["Gurpartap Singh"]
-  gem.email         = ["contact@gurpartap.com"]
+  gem.authors       = ["Gurpartap Singh", "Charcoal"]
+  gem.email         = ["contact@gurpartap.com", "art@charcoal-se.org"]
   gem.description   = "Simple AES encryption / decryption for Ruby"
   gem.summary       = "AESCrypt is a simple to use, opinionated AES encryption / decryption Ruby gem that just works."
   gem.homepage      = "http://github.com/Gurpartap/aescrypt"
--- lib/aescrypt.rb
+++ lib/aescrypt.rb
@@ -55,7 +55,7 @@ module AESCrypt
   #:arg: iv => String
   #:arg: cipher_type => String
   def self.decrypt_data(encrypted_data, key, iv, cipher_type)
-    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes = OpenSSL::Cipher.new(cipher_type)
     aes.decrypt
     aes.key = key
     aes.iv = iv if iv != nil
@@ -74,7 +74,7 @@ module AESCrypt
   #:arg: iv => String
   #:arg: cipher_type => String  
   def self.encrypt_data(data, key, iv, cipher_type)
-    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes = OpenSSL::Cipher.new(cipher_type)
     aes.encrypt
     aes.key = key
     aes.iv = iv if iv != nil
